44fe7c0516319d208b391cbd98d874936bbcf78a
Pooya Hatami <am.hatami@gmail.com>
2018-11-27 01:11:13 -0800

    done

---
+++ IOS-Swift-ARkitFaceTrackingNose01/FaceNode.swift
@@ -0,0 +1,51 @@
+//
+//  FaceNode.swift
+//  IOS-Swift-ARkitFaceTrackingNose01
+//
+//  Created by Pooya on 2018-11-27.
+//  Copyright © 2018 Soonin. All rights reserved.
+//
+
+import SceneKit
+
+class FaceNode: SCNNode {
+    
+    var options: [String]
+    var index = 0
+    
+    init(with options: [String], width: CGFloat = 0.06, height: CGFloat = 0.06) {
+        self.options = options
+        
+        super.init()
+        
+        let plane = SCNPlane(width: width, height: height)
+        plane.firstMaterial?.diffuse.contents =  UIImage(named: options.first!)
+        plane.firstMaterial?.isDoubleSided = true
+        
+        geometry = plane
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Custom functions
+
+extension FaceNode {
+    
+    func updatePosition(for vectors: [vector_float3]) {
+        let newPos = vectors.reduce(vector_float3(), +) / Float(vectors.count)
+        position = SCNVector3(newPos)
+    }
+    
+    func next() {
+        index = (index + 1) % options.count
+        
+        if let plane = geometry as? SCNPlane {
+            plane.firstMaterial?.diffuse.contents = UIImage(named: options[index])
+            plane.firstMaterial?.isDoubleSided = true
+        }
+    }
+}
+
--- IOS-Swift-ARkitFaceTrackingNose01/ViewController.swift
+++ IOS-Swift-ARkitFaceTrackingNose01/ViewController.swift
@@ -7,14 +7,97 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    let noseOptions = ["nose01", "nose02", "nose03", "nose04", "nose05", "nose06", "nose07", "nose08", "nose09"]
+    let features = ["nose"]
+    var featureIndices = [[6]]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sceneView.delegate = self
     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuratio = ARFaceTrackingConfiguration()
+        
+        sceneView.session.run(configuratio)
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: sceneView)
+        let results = sceneView.hitTest(location, options: nil)
+        if let result = results.first,
+            let node = result.node as? FaceNode {
+            node.next()
+        }
+    }
+    
+    
+    
+    func updateFeatures(for node: SCNNode, using anchor: ARFaceAnchor) {
+        print(featureIndices)
+        for (feature, indices) in zip(features, featureIndices) {
+            let child = node.childNode(withName: feature, recursively: false) as? FaceNode
+            let vertices = indices.map { anchor.geometry.vertices[$0] }
+            child?.updatePosition(for: vertices)
+        }
+    }
+    
+}
 
-
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+        let device: MTLDevice!
+        device = MTLCreateSystemDefaultDevice()
+        guard let faceAnchor = anchor as? ARFaceAnchor else {
+            return nil
+        }
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let node = SCNNode(geometry: faceGeometry)
+                node.geometry?.firstMaterial?.fillMode = .lines
+        //node.geometry?.firstMaterial?.transparency = 0.0
+        
+        let noseNode = FaceNode(with: noseOptions)
+        noseNode.name = "nose"
+        node.addChildNode(noseNode)
+        
+        updateFeatures(for: node, using: faceAnchor)
+        
+        return node
+    }
+    
+    func renderer(
+        _ renderer: SCNSceneRenderer,
+        didUpdate node: SCNNode,
+        for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+            let faceGeometry = node.geometry as? ARSCNFaceGeometry else {
+                return
+        }
+        
+        faceGeometry.update(from: faceAnchor.geometry)
+        updateFeatures(for: node, using: faceAnchor)
+    }
+    
+    
 }
 
